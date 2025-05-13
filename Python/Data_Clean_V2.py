e0fae165c231c44cba74e3cb3199bb3b06d43671
emminst <emminst5381@gmail.com>
2025-05-13 15:04:58 +0100

    upload files

--- Python/Data_Clean_V2.py
+++ Python/Data_Clean_V2.py
@@ -1,13 +1,18 @@
 import pandas as pd
+
 df = pd.read_csv('Python/Data/03_Library Systembook.csv')
-df.fillna('n/a')
 df_clean = df.dropna()
-df_clean.loc[df_clean['Book checkout'].notna(), 'Book checkout'] = df_clean['Book checkout'].str.replace('"', '')
 
-data_enriched = df_clean
+"""
+df_clean.drop(16, axis=0, inplace=True)
 
+df_clean['Book checkout'] = pd.to_datetime(df_clean['Book checkout'],format, dayfirst=True)
+df_clean['Book Returned'] = pd.to_datetime(df_clean['Book Returned'],format, dayfirst=True)
+
+data_enriched = df_clean.copy()
 data_enriched['loan_duration'] = (data_enriched['Book Returned'] - data_enriched['Book checkout']).dt.days
 valid_loan_data = data_enriched[data_enriched['loan_duration']>=0]
+"""
 
 from sqlalchemy import create_engine
 server = 'localhost'
@@ -16,5 +21,9 @@ driver = 'ODBC Driver 17 for SQL Server'
 connection_string = f'mssql+pyodbc://@{server}/{database}?driver={driver}'
 engine = create_engine(connection_string)
 
-df = pd.DataFrame(df_valid_loan_data)
+#df = pd.DataFrame(valid_loan_data)
+df = pd.DataFrame(df_clean)
+
 df.to_sql('Books', engine, if_exists='replace', index=False)
+
+df.to_csv('Python/Data/cleaned_dataset.csv',index=False)
