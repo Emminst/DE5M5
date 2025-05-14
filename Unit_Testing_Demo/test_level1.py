b5e8026ee9cdcd23554e9e06b4c82ce30757f1da
emminst <emminst5381@gmail.com>
2025-05-14 10:22:06 +0100

    upload files

--- Unit_Testing_Demo/test_level1.py
+++ Unit_Testing_Demo/test_level1.py
@@ -3,21 +3,32 @@ from calculator_app import Calculator
 
 class TestOperations(unittest.TestCase):
 
+    def setUp(self):
+        self.calculation=Calculator(8,2)  #removes the need for repeating this in every test below
+
     def test_sum(self):
-        calculation = Calculator(8,2)
-        self.assertEqual(calculation.do_sum(),10,'The sum is wrong (not 10)!')
+        #calculation = Calculator(8,2)
+        #self.assertEqual(calculation.do_sum(),10,'The sum is wrong (not 10)!')
+        #but need to add 'self.' before calculation now:
+        self.assertEqual(self.calculation.do_sum(),10,'The sum is wrong (not 10)!')
 
     def test_product(self):
-        calculation = Calculator(8,2)
-        self.assertEqual(calculation.do_product(),16,'The product is wrong (not 16)!')   
+        #calculation = Calculator(8,2)
+        #self.assertEqual(calculation.do_product(),16,'The product is wrong (not 16)!')   
+        #but need to add 'self.' before calculation now:
+        self.assertEqual(self.calculation.do_product(),16,'The product is wrong (not 16)!')
 
     def test_subtract(self):
-        calculation = Calculator(8,2)
-        self.assertEqual(calculation.do_subtract(),6,'The difference is wrong (not 6)!')         
+        #calculation = Calculator(8,2)
+        #self.assertEqual(calculation.do_subtract(),6,'The difference is wrong (not 6)!')         
+        #but need to add 'self.' before calculation now:
+        self.assertEqual(self.calculation.do_subtract(),6,'The difference is wrong (not 6)!')
 
     def test_divide(self):
-        calculation = Calculator(8,2)
-        self.assertEqual(calculation.do_divide(),4,'The quotient is wrong (not 4)!')    
+        #calculation = Calculator(8,2)
+        #self.assertEqual(calculation.do_divide(),4,'The quotient is wrong (not 4)!')   
+        #but need to add 'self.' before calculation now:
+        self.assertEqual(self.calculation.do_divide(),4,'The quotient is wrong (not 4)!')
 
 if __name__ == '__main__':
     unittest.main()
